c19aa69b0e825fb45f5004b4c15197cdc8562013
Giorgiana353 <giorgiana.dinu@emanuel.ro>
2024-01-18 17:49:42 +0200

    adunarea

---
+++ Adunarea/Adunarea/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Adunarea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int s;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            s = a + b;
+
+            Console.WriteLine(s);
+
+                
+        }
+    }
+}
